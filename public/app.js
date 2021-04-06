bfc9735651e94e493aa2e7e52fb96d5b7deaebd6
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-04-06 18:51:48 -0500

    Connect the form to the API

--- public/app.js
+++ public/app.js
@@ -1,18 +1,42 @@
+const { createTodo } = require("../controllers/todos")
+
 $(document).ready(() => {
     $.getJSON("/api/todos")
         .then(addTodos)
         .catch(err => {
             console.log(err)
         })
+    $('#todoInput').keypress(event => {
+        if (event.which == 13) {
+            createTodo();
+        }
+    })
 })
 
 const addTodos = todos => {
     // add todos to page here
     todos.forEach(todo => {
-        const newTodo = $('<li class="task">' + todo.name + '</li>')
-        if (todo.completed) {
-            newTodo.addClass('done')
-        }
-        $('.list').append(newTodo)
+        addTodo(todo);
     })
+}
+
+const addTodo = todo => {
+    const newTodo = $('<li class="task">' + todo.name + '</li>')
+    if (todo.completed) {
+        newTodo.addClass('done')
+    }
+    $('.list').append(newTodo)
+}
+
+const createTodo = () => {
+    // send request to create new todo
+    const userInput = $('#todoInput').val()
+    $.post('/api/todos', { name: userInput })
+        .then(newTodo => {
+            $('#todoInput').val('')
+            addTodo(newTodo)
+        })
+        .catch(err => {
+            console.log(err)
+        })
 }
