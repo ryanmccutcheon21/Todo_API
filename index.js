01688390af694e73be6e4dc700dd2b055e5371a3
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-04-06 21:54:17 -0500

    Fixed bugs where styling was not connected

--- index.js
+++ index.js
@@ -7,16 +7,26 @@ const
 
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + 'public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname, + '/public'));
 app.use(express.static(__dirname + '/views'));
 
+
+app.use('/api/todos', todoRoutes);
 
 app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
 
-app.use('/api/todos', todoRoutes);
+app.all('*', (req, res, next) => {
+    res.send('Page Not Found', 404)
+});
+
+app.use((err, req, res, next) => {
+    const { status = 500 } = err;
+    if (!err.message) err.message = 'Oh no! Something went wrong!'
+    res.status(status).render('error', { err })
+});
 
 app.listen(port, () => {
     console.log(`App is running on port ${port}`);
--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.set('debud', true);
+// mongoose.set('debud', true);
 
 mongoose.connect('mongodb://localhost/todo-api');
 
